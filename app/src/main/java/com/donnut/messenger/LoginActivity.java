dc5e72326b6f89921e7c2462713b15bcd9fa7e31
fdonnut <36168787+fdonnut@users.noreply.github.com>
2023-01-09 18:24:20 +0300

    reset password activity

--- app/src/main/java/com/donnut/messenger/LoginActivity.java
+++ app/src/main/java/com/donnut/messenger/LoginActivity.java
@@ -32,7 +32,8 @@ public class LoginActivity extends AppCompatActivity {
         textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = ResetPasswordActivity.newIntent(LoginActivity.this, editTextEmail.getText().toString().trim());
+                startActivity(intent);
             }
         });
         textViewRegister.setOnClickListener(new View.OnClickListener() {
