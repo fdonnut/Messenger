7e8d0ba62449d1970e8a5c05d5250479edda8adb
fdonnut <36168787+fdonnut@users.noreply.github.com>
2023-01-09 17:59:54 +0300

    registration activity

---
+++ app/src/main/java/com/donnut/messenger/RegistrationActivity.java
@@ -0,0 +1,54 @@
+package com.donnut.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextName;
+    private EditText editTextLastName;
+    private EditText editTextAge;
+    private Button buttonSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        initViews();
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = getTrimmedValue(editTextEmail);
+                String password = getTrimmedValue(editTextPassword);
+                String name = getTrimmedValue(editTextName);
+                String lastName = getTrimmedValue(editTextLastName);
+                int age = Integer.parseInt(getTrimmedValue(editTextAge));
+            }
+        });
+    }
+
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAge = findViewById(R.id.editTextAge);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+    }
+
+    private String getTrimmedValue(EditText editText) {
+        return editText.getText().toString().trim();
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, RegistrationActivity.class);
+    }
+}
