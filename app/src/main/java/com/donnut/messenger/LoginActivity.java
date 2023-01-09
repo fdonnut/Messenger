4a25dd394a573be59af3736585de9132958b1bd8
fdonnut <36168787+fdonnut@users.noreply.github.com>
2023-01-09 19:14:02 +0300

    login view model

--- app/src/main/java/com/donnut/messenger/LoginActivity.java
+++ app/src/main/java/com/donnut/messenger/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.donnut.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,16 +22,25 @@ public class LoginActivity extends AppCompatActivity {
     private TextView textViewForgotPassword;
     private TextView textViewRegister;
 
+    private LoginViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initViews();
+        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        observeViewModel();
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
+                viewModel.login(email, password);
             }
         });
         textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +55,27 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = RegistrationActivity.newIntent(LoginActivity.this);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(LoginActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    Intent intent = UsersActivity.newIntent(LoginActivity.this);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
     }
---
+++ app/src/main/java/com/donnut/messenger/UsersActivity.java
@@ -0,0 +1,20 @@
+package com.donnut.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, UsersActivity.class);
+    }
+}
